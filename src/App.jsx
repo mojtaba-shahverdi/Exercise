204fcc9773b1bf5804f16e3d9c9e8f7255d2fc74
moji <mojtaba0shahverdi@gmail.com>
2023-01-07 13:56:12 +0330

    INITIALIZING

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Box } from '@mui/material'
+import './App.css'
+import Navbar from './components/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import ExerciseDetail from './pages/ExerciseDetail'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
+  )
+}
+
+export default App
